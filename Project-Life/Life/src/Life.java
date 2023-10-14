c4a130537cf4dd0f339cf07a203b5a4d22646953
Bruno-Pinto <bpintodasilva@gmail.com>
2023-10-14 11:17:47 +0200

    Added JFrame class
    Added JPanel Class

--- Project-Life/Life/src/Life.java
+++ Project-Life/Life/src/Life.java
@@ -1,9 +1,48 @@
 
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Life {
-
+	
+	//setup the game
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int h = 2;
+		int w = 2;
+		int[][] grid1 = new int[h][w];
+		int[][] grid2 = new int[h][w];
+		int flip = 0;
+		grid1 = randomFill(grid1, w, h);
+		
+		// print(grid1, h, w);
 
+		MyFrame myFrame = new MyFrame();
+		
+		
 	}
-
+	//print the grid in console
+	static void print(int[][] grid, int h, int w) {
+		for (int i=0; i<h; i++) {
+			for (int j=0; j<w; j++) {
+				System.out.print(grid[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	//fill the grid with random 0 or 1
+	static int[][] randomFill(int[][] grid, int w, int h){
+		Random random = new Random();
+		for (int i=0; i<h; i++) {
+			for (int j=0; j<w; j++) {
+				grid[i][j] = random.nextInt(2);
+				
+			}
+		}
+		return grid;
+	}
 }
---
+++ Project-Life/Life/src/MyFrame.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+
+	MyPanel panel;
+	
+	MyFrame(){
+		
+		panel = new MyPanel();
+		
+		this.add(panel);
+		this.pack();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setTitle("Life");
+		this.setLocationRelativeTo(null);
+		//this.getContentPane().setBackground(Color.black);		
+		this.setVisible(true);
+		
+	}
+	
+}
